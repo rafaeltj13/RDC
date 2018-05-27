04a548887ffcfcb9207fad9fe69f7ccded85a6e9
Rafael <rafael.maciel@ccc.ufcg.edu.br>
2018-05-27 18:09:59 -0300

    Adding migrations

---
+++ Infrastructure/Mapping/RecipeMapping.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Mapping
+{
+    public class RecipeMapping : IEntityTypeConfiguration<Recipe>
+    {
+        public void Configure(EntityTypeBuilder<Recipe> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
---
+++ Infrastructure/Repository/DataContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Infrastructure.Repository
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DataContext>();
+
+            var connectionString = configuration.GetConnectionString("App");
+
+            builder.UseSqlServer(connectionString);
+
+            return new DataContext(builder.Options);
+        }
+    }
+}
